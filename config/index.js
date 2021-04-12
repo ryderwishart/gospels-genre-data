b2edb8c8dcf8f95150215ab51fb5626c415e171e
ryderwishart <ryderwishart@gmail.com>
2021-04-12 09:54:43 -0700

    Adds trailing slash to config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 const dev = process.env.NODE_ENV !== 'production';
 
+console.log(process.env.NODE_ENV)
 export const server = dev 
     ? 'http://localhost:1337'
-    : 'https://opentext.vercel.app'
+    : 'https://opentext.vercel.app/'
 
 // export const server = 'http://localhost:1337'
