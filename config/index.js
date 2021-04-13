803292db5a8f992ac0b07c21b303ff5b84ed0acf
ryderwishart <ryderwishart@gmail.com>
2021-04-13 16:18:37 -0700

    fix(config): force live domain
    May not work: may require api-only deploy first

--- config/index.js
+++ config/index.js
@@ -4,5 +4,5 @@ const dev = process.env.NODE_ENV !== 'production';
 //     ? 'http://localhost:1337'
 //     : 'https://opentext.vercel.app/'
 
-export const server = 'http://localhost:1337'
-// export const server = 'https://opentext.vercel.app/'
+// export const server = 'http://localhost:1337'
+export const server = 'https://opentext.vercel.app/'
