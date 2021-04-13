eff88ea5971324500067ec4aec0a300841b5abdb
ryderwishart <ryderwishart@gmail.com>
2021-04-13 08:06:19 -0700

    fix(client [text]): try to catch api errors and return null in those cases

--- pages/johannine/[text].js
+++ pages/johannine/[text].js
@@ -4,39 +4,43 @@ import styles from '../../styles/Home.module.css'
 
 const TextPage = props => {
     console.log(props)
-    const text = props.response.text;
-    return (
-        <div className={styles.container}>
+    const text = props.response?.text;
+    if(!!text) {
 
-        <main className={styles.main}>
-            <h1>{text.key}</h1>
-            <div className={styles.grid}>
-                {/* text.content.turn.content.map(stageContainer).stageContainer.content.map(moveContainer).expressions || .meanings */
-                    text.content.turn.content.map(stageContainer => {
-                        return (
-                                    <div className={styles.card}>
-                            <Link href={`/johannine/${props.currentText}/${stageContainer.stage.key}`}>
-                                <a>
-                                        <h2>Stage: [need stage titles in data 🤓]</h2>
-                                        
-                                                {stageContainer.stage.key} {/* TODO: again, these stage slugs should be based on the first episode title, if multiple, and on the id only as a fallback */}
-                                </a>
-                            </Link>
-                                    </div>
-                        )
-                    })
-                }
+        return (
+            <div className={styles.container}>
+    
+            <main className={styles.main}>
+                <h1>{text.key}</h1>
+                <div className={styles.grid}>
+                    {/* text.content.turn.content.map(stageContainer).stageContainer.content.map(moveContainer).expressions || .meanings */
+                        text.content.turn.content.map(stageContainer => {
+                            return (
+                                        <div className={styles.card}>
+                                <Link href={`/johannine/${props.currentText}/${stageContainer.stage.key}`}>
+                                    <a>
+                                            <h2>Stage: [need stage titles in data 🤓]</h2>
+                                            
+                                                    {stageContainer.stage.key} {/* TODO: again, these stage slugs should be based on the first episode title, if multiple, and on the id only as a fallback */}
+                                    </a>
+                                </Link>
+                                        </div>
+                            )
+                        })
+                    }
+                </div>
+            </main>
+            <footer className={styles.footer}>
+                    <Link href={`/johannine`}>
+                        <a className={styles.card}>
+                        &larr; Back to all Johnannine texts
+                        </a>
+                    </Link>
+            </footer>
             </div>
-        </main>
-        <footer className={styles.footer}>
-                <Link href={`/johannine`}>
-                    <a className={styles.card}>
-                    &larr; Back to all Johnannine texts
-                    </a>
-                </Link>
-        </footer>
-        </div>
-    )
+        )
+    }
+    return null;
 }
 
 export default TextPage;
@@ -58,9 +62,17 @@ export async function getStaticProps(context) {
 }
 
 export const getStaticPaths = async () => {
-    const response = await (
-        await fetch(`${server}/api/johannine`)
-    ).json()
+    let response;
+    try {
+        response = await (
+            await fetch(`${server}/api/johannine/`)
+        ).json()
+     } catch(error) {
+        response = null;
+     }
+    // const response = await (
+    //     await fetch(`${server}/api/johannine/`)
+    // ).json()
     const textKeys = response?.data.map(textContainer => textContainer.text.key)
     const paths = textKeys?.map(textKey => ({params: { text: textKey.toString()}}))
     return {
