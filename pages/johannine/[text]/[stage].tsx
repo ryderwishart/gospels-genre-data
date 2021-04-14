e45bfc99cecfdf1ede4fd01f7377fff06b2056b0
ryderwishart <ryderwishart@gmail.com>
2021-04-13 20:41:53 -0700

    fix: type error

--- pages/johannine/[text]/[stage].tsx
+++ pages/johannine/[text]/[stage].tsx
@@ -119,10 +119,10 @@ const Stage: React.FC<any> = props => {
                     ellipsis: true,
                     render: (move) => {
                         console.log(move)
-                        const instanceArrays: ChoiceContainer[] = move.meanings.filter((systemContainer: SystemContainer) => systemContainer.system.key === system)
+                        const instanceArrays: ChoiceContainer[][] = move.meanings.filter((systemContainer: SystemContainer) => systemContainer.system.key === system)
                             .map((systemContainer: SystemContainer) => systemContainer.system.instances)
                         console.log({instanceArrays})
-                        const choices = instanceArrays.length > 0 && instanceArrays.map((instances: ChoiceContainer) => {
+                        const choices = instanceArrays.length > 0 && instanceArrays.map((instances) => {
                             return instances.map(instance => instance.choice)
                         })
                         console.log(choices[0])
