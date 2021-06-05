36bad481921d048e7c887af4dacb588f481fcb79
ryderwishart <ryderwishart@gmail.com>
2021-06-05 13:54:55 -0700

    api(episodes/[title]): adds graph data to existing API endpoint and new dependency
    Adds react-force-graph dependency

--- pages/api/episodes/[title].js
+++ pages/api/episodes/[title].js
@@ -1,22 +1,45 @@
 import episodesFeatures from '../../../public/data/stages/episodesDynamicFeatures.json';
 import { getFirstTitleHyphenatedLowerCaseStringFromTitleString } from '../../../functions/getFirstTitleHyphenatedLowerCaseStringFromTitleString';
+import nodes from '../../../public/data/graphData/episodeNodes.json';
+import edges from '../../../public/data/graphData/episodeEdges.json';
 
 const handler = (req, res) => {
   const title = req.query.title;
   let currentEpisode = null;
   let previousEpisode = null;
   let nextEpisode = null;
+  let similarNodes = null;
+  let similarEdges = null;
 
   try {
     episodesFeatures.find((episode, index) => {
       const formattedEpisodeTitle = getFirstTitleHyphenatedLowerCaseStringFromTitleString(
         { string: episode.title },
       );
+      const edgeIDReferences = [];
       if (formattedEpisodeTitle.includes(title)) {
+        const episodeIDAndTitle = `${episode.episode} ${episode.title}`
+          .split(/s ,'".;’“”/)
+          .join('');
         // TODO: This query could be easily adapted for any episodes that include a given feature or that do NOT include a given feature
-        (currentEpisode = episode),
-          (previousEpisode = episodesFeatures[index - 1]),
-          (nextEpisode = episodesFeatures[index + 1]);
+        currentEpisode = episode;
+        previousEpisode = episodesFeatures[index - 1];
+        nextEpisode = episodesFeatures[index + 1];
+
+        similarEdges = edges.filter((edge) => {
+          const edgeIDs = edge.ID.split('-').join(' ');
+          if (edgeIDs.includes(episodeIDAndTitle)) {
+            edgeIDReferences.push(edge.SOURCE);
+            edgeIDReferences.push(edge.TARGET);
+            return edge;
+          }
+        });
+
+        similarNodes = nodes.filter((node) => {
+          if (edgeIDReferences.includes(node.ID)) {
+            return node;
+          }
+        });
       } else {
         return null;
       }
@@ -31,6 +54,8 @@ const handler = (req, res) => {
       currentEpisode,
       previousEpisode,
       nextEpisode,
+      similarEdges,
+      similarNodes,
     });
   } else {
     res.status(404).json({
