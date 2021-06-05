3024b265e08519be74ff3ada45518a44479c2212
ryderwishart <ryderwishart@gmail.com>
2021-06-04 18:30:54 -0700

    refactor: prunes unused files

--- functions/getMostSimilarVectors.ts
+++
@@ -1,9 +0,0 @@
-export const getMostSimilarVectors = ({queriedVector, vectorsDataSet, returnTopN=10}): (string|number)[] => {
-    const topNIndexes = queriedVector.filter((cell, index) => [cell, index])
-    const vectorsDataSet.filter(row[0])  matches queriedVector[0]
-    // 
-    const mostSimilarVectors = matches.map(match => [match[0], ])
-        return mostSimilarVectors;
-    // else message.error('queried vector not found in vectors. Ensure the first item in the vector is a string (e.g. an ID or a title to identify the vector)
-    
-}
--- pages/api/texts/index.js
+++
@@ -1,7 +0,0 @@
-import root from '../../../public/data/texts/Nestle1904.xml';
-
-const handler = (req, res) => {
-  res.status(200).json(root);
-};
-
-export default handler;
