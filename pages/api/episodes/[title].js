f8192767fa130852fc4de48d57e2d70fdeefad1b
ryderwishart <ryderwishart@gmail.com>
2021-06-10 17:23:14 -0700

    cleanup

--- pages/api/episodes/[title].js
+++ pages/api/episodes/[title].js
@@ -14,7 +14,6 @@ const handler = (req, res) => {
   let similarEdges = null;
   let currentEpisodeMetaData = null;
   episodesMetaData.root.episode.find((episodeContainer) => {
-    console.log(episodeContainer);
     const formattedEpisodeTitle = getFirstTitleHyphenatedLowerCaseStringFromTitleString(
       { string: episodeContainer.$.title },
     );
