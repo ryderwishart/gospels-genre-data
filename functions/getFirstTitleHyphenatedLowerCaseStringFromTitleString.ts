ea80a8bebcea20ac1aceed2c5b652606e17543c1
ryderwishart <ryderwishart@gmail.com>
2021-06-11 13:07:08 -0700

    fix(getFirstTitleHyphenatedLowerCaseStringFromTitleString): properly removes possessive apostrophes from episode names

--- functions/getFirstTitleHyphenatedLowerCaseStringFromTitleString.ts
+++ functions/getFirstTitleHyphenatedLowerCaseStringFromTitleString.ts
@@ -6,14 +6,17 @@ export const getFirstTitleHyphenatedLowerCaseStringFromTitleString = (
   props: functionProps,
 ): string => {
   try {
-    const firstTitle = props.string.split(/[,'".;’“”]+/)[0];
-    return firstTitle
-      .split(' ')
-      .flat(2)
-      .map((word) => word.toLowerCase())
-      .join('-')
-      .split(/[,'".;’“”]+/)
-      .join('');
+    const firstTitle = props.string.split(/[,;]+/)[0]; // get first of multi-title episodes separated by comma or semicolon
+    return (
+      firstTitle
+        .split(' ') // split by word
+        .flat(2) // flatten to one list
+        .map((word) => word.toLowerCase())
+        .join('-') // turn into an seo-friendly url string
+        .split(/[,'".;’’“”]+/) // remove special characters
+        // TODO: use a regex to remove non-letter or hyphen characters
+        .join('') // get one string
+    );
   } catch (error) {
     throw new Error(
       'getFirstTitleHyphenatedLowerCaseStringFromTitleString did not receive a valid string, or a string was passed directly instead of an object. Did you mean to use getFirstTitleHyphenatedLowerCaseStringFromTitleString({string: /* string you wished to pass */})',
