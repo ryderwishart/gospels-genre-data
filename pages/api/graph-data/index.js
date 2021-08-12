2d6f6beae5e801ffd0a757fb46b4a3e9d179fdf0
ryderwishart <ryderwishart@gmail.com>
2021-08-11 21:45:17 -0700

    fix(graph-data): reverts api route from TS to JS

--- pages/api/graph-data/index.js
+++ pages/api/graph-data/index.js
@@ -1,7 +1,6 @@
 import graphData from '../../../public/data/graphData/graphData.json';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const { nodes, edges }: any = graphData;
+const { nodes, edges } = graphData;
 
 const handler = (req, res) => {
   res.status(200).send({ nodes, edges });
