3f27e6ae47d4e1860b167fe83b4e0692494c414c
ryderwishart <ryderwishart@gmail.com>
2021-09-07 22:41:15 -0700

    fix(types): reinstate some deprecated types to allow build to pass

--- types/index.ts
+++ types/index.ts
@@ -1,64 +1,66 @@
 // DEPRECATED:
 
-// export interface TextContainer {
-//   text: Text;
-// }
-// export interface Text {
-//   key: string;
-//   content: TurnContainer;
-// }
+export interface TextContainer {
+  text: Text;
+}
+export interface Text {
+  key: string;
+  content: TurnContainer;
+}
 
-// export interface TurnContainer {
-//   turn: Turn;
-// }
-// export interface Turn {
-//   key: string;
-//   content: StageContainer[];
-// }
+export interface TurnContainer {
+  turn: Turn;
+}
+export interface Turn {
+  key: string;
+  content: StageContainer[];
+}
 
-// export interface StageContainer {
-//   stage: Stage;
-// }
-// export interface Stage {
-//   key: string;
-//   content: MoveContainer[];
-// }
+export interface StageContainer {
+  stage: Stage;
+}
+export interface Stage {
+  key: string;
+  content: MoveContainer[];
+}
 
-// export interface MoveContainer {
-//   move: Move;
-// }
-// export interface Move {
-//   key: string;
-//   expressions: WordContainer[];
-//   meanings: SystemContainer[];
-// }
+export interface MoveContainer {
+  move: Move;
+}
+export interface Move {
+  key: string;
+  expressions: WordContainer[];
+  meanings: SystemContainer[];
+}
 
-// export interface WordContainer {
-//   word: Word;
-// }
-// export interface Word {
-//   key: string;
-//   lemma: string;
-//   norm: string;
-//   string: string;
-// }
+export interface WordContainer {
+  word: Word;
+}
+export interface Word {
+  key: string;
+  lemma: string;
+  norm: string;
+  string: string;
+}
 
-// export interface SystemContainer {
-//   system: System;
-// }
+export interface SystemContainer {
+  system: System;
+}
 
-// export interface System {
-//   key: string;
-//   instances: ChoiceContainer[];
-// }
+export interface System {
+  key: string;
+  instances: ChoiceContainer[];
+}
 
-// export interface ChoiceContainer {
-//   choice: Choice;
-// }
-// export interface Choice {
-//   key: string;
-//   realization: string; // NOTE: This string is like a stringified array ("N1904.John.9.1.2' 'N1904.John.9.1.3") Basically it should be split on `' '`
-// }
+export interface ChoiceContainer {
+  choice: Choice;
+}
+export interface Choice {
+  key: string;
+  realization: string; // NOTE: This string is like a stringified array ("N1904.John.9.1.2' 'N1904.John.9.1.3") Basically it should be split on `' '`
+}
+
+// END DEPRECATED
 
 // SITUATION FEATURES
 
