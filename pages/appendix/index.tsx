40e8911e7e06418fb7111232635d55df3897d120
ryderwishart <ryderwishart@gmail.com>
2021-09-15 07:42:06 -0700

    feat(appendix/index): clarifies each button with description and icon
    - adds link to additional, most recent appendix

--- pages/appendix/index.tsx
+++ pages/appendix/index.tsx
@@ -1,35 +1,105 @@
 import Layout from '../../components/Layout';
 import Link from 'next/link';
 import styles from '../../styles/Home.module.css';
+import { ApartmentOutlined, PictureOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
 
 export default function Appendices() {
   return (
     <Layout pageTitle="Appendices">
-      <div className={styles.card}>
-        <Link href="/appendix/principal-components">
-          <a>Episode Principal Component Scores</a>
-        </Link>
-      </div>
-      <div className={styles.card}>
-        <Link href="/appendix/principal-component-features">
-          <a>Principal Component Feature Correlations</a>
-        </Link>
-      </div>
-      <div className={styles.card}>
-        <Link href="/appendix/episodes-graph">
-          <a>Graph of All Episode Situation Similarities</a>
-        </Link>
-      </div>
-      <div className={styles.card}>
-        <Link href="/appendix/feature-sets">
-          <a>Grammatical Features by Order of Text</a>
-        </Link>
-      </div>
-      <div className={styles.card}>
-        <Link href="/appendix/episode-similarity-by-grammar">
-          <a>Episode Cosine Similarity by Grammatical Features</a>
-        </Link>
-      </div>
+      <Link href="/appendix/principal-components">
+        <a>
+          <div className={styles.card}>
+            <Tooltip title="Contextual Data Table">
+              <PictureOutlined />
+            </Tooltip>
+            {'  '}
+            Episode Principal Component Scores
+            <br />
+            <span style={{ color: 'black' }}>
+              All of the principal-component scores for each episode
+            </span>
+          </div>
+        </a>
+      </Link>
+      <Link href="/appendix/principal-component-features">
+        <a>
+          <div className={styles.card}>
+            <Tooltip title="Contextual Data Table">
+              <PictureOutlined />
+            </Tooltip>
+            {'  '}
+            Principal Component Feature Correlations
+            <br />
+            <span style={{ color: 'black' }}>
+              How much each feature correlates with each principal component
+            </span>
+          </div>
+        </a>
+      </Link>
+      <Link href="/appendix/episodes-graph">
+        <a>
+          <div className={styles.card}>
+            <Tooltip title="Contextual Data Table">
+              <PictureOutlined />
+            </Tooltip>
+            {'  '}
+            Graph of All Episode Situation Similarities
+            <br />
+            <span style={{ color: 'black' }}>
+              A Graph of all episode clusters given a user-specified similarity
+              threshold
+            </span>
+          </div>
+        </a>
+      </Link>
+      <Link href="/appendix/feature-sets">
+        <a>
+          <div className={styles.card}>
+            <Tooltip title="Grammatical Data Table">
+              <ApartmentOutlined rotate={270} />
+            </Tooltip>
+            {'  '}
+            Grammatical Features by Order of Text
+            <br />
+            <span style={{ color: 'black' }}>
+              Tallies of all grammatical features for each order of text
+            </span>
+          </div>
+        </a>
+      </Link>
+      <Link href="/appendix/episode-similarity-by-grammar">
+        <a>
+          <div className={styles.card}>
+            <Tooltip title="Grammatical and Contextual Data Table">
+              <PictureOutlined /> <ApartmentOutlined rotate={270} />
+            </Tooltip>
+            {'  '}
+            Episode Cosine Similarity by Grammatical Features
+            <br />
+            <span style={{ color: 'black' }}>
+              Degree of similarity between every episode based on grammatical
+              probabilities
+            </span>
+          </div>
+        </a>
+      </Link>
+      <Link href="/appendix/cluster-to-episode-similarity">
+        <a>
+          <div className={styles.card}>
+            <Tooltip title="Grammatical and Contextual Data Table">
+              <PictureOutlined /> <ApartmentOutlined rotate={270} />
+            </Tooltip>
+            {'  '}
+            Cluster-to-Episode Cosine Similarity by Grammatical Features
+            <br />
+            <span style={{ color: 'black' }}>
+              Degree of similarity between clusters and episodes based on
+              grammatical probabilities
+            </span>
+          </div>
+        </a>
+      </Link>
     </Layout>
   );
 }
