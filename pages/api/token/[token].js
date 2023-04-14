42f5ab87c22dfea9a64a9fbaa43aac6e6bdc25b1
ryderwishart <ryderwishart@gmail.com>
2023-04-14 13:09:20 -0700

    Add token endpoint

---
+++ pages/api/token/[token].js
@@ -0,0 +1,39 @@
+import situationsFeatures from '../../../public/data/stages/situations_data.json';
+
+const findSituationByURL = (urlValue) => {
+  return situationsFeatures.find((situation) => {
+    if (situation.token_ids.includes(urlValue)) {
+      return true;
+    }
+
+    return situation.token_refs.some((ref) => ref.includes(urlValue));
+  });
+};
+
+const handler = (req, res) => {
+  console.log('req.query', req.query)
+  const urlValue = req.query.token;
+
+  try {
+    const matchingSituation = findSituationByURL(urlValue);
+
+    if (matchingSituation !== undefined) {
+      res.status(200).json({ matchingSituation });
+    } else {
+      res.status(404).json({
+        message: `Situation with URL value '${urlValue}' not found.`,
+      });
+    }
+  } catch (error) {
+    console.warn(
+      'Encountered an error trying to match the URL query string with a situation',
+      req.query,
+      { error }
+    );
+    res.status(500).json({
+      message: `An error occurred while searching for the situation with URL value '${urlValue}'.`,
+    });
+  }
+};
+
+export default handler;
